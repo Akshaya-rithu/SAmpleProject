83a8d2b3d74ad86f30b8db7a388b2a5b31de4d5b
Akshaya-rithu <akshaya02rithu@gmail.com>
2020-06-01 07:26:59 +0530

    my first commit

---
+++ src/org/test/SourceFile.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class SourceFile {
+
+	public static void main(String[] args) {
+	System.out.println("Hai");
+	System.out.println("Hello");
+	System.out.println("GITHUB");
+	}
+
+}
